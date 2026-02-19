3c0f302aa69f8cca2a832e06cfc9244940236ea5
Fernanda Beckman <ferbeckman27@gmail.com>
2026-02-18 22:06:04 -0300

    Atualizações

--- src/pages/ClientDashboard.tsx
+++ src/pages/ClientDashboard.tsx
@@ -206,7 +206,7 @@ useEffect(() => {
       <header className="border-b border-white/10 bg-black/60 p-4 sticky top-0 z-50 backdrop-blur-md">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
           <div className="flex flex-col items-center">
-            <img src="/media/logo-arena.png" alt="Logo" className="w-60 h-60 object-contain" />
+            <img src="/media/logo-arena.png" alt="Logo" className="w-40 h-40 object-contain" />
           <span className="text-[20px] font-black uppercase text-[#22c55e] tracking-[0.2em]">BEM VINDO AO PAINEL DO CLIENTE</span>
         </div>
         </div>
